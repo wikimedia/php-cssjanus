62a9c32e6e140de09082b40a6e99d868ad14d4e0
Matthew Flaschen <matthew.flaschen@gatech.edu>
2014-10-08 19:07:36 -0400

    Add !important support for four-value rules
    The regex change was given by Bartosz Dziewoński at
    https://bugzilla.wikimedia.org/show_bug.cgi?id=61440#c1 .

--- test/suites/CSSJanusTest.php
+++ test/suites/CSSJanusTest.php
@@ -52,7 +52,7 @@ class CSSJanusTest extends PHPUnit_Framework_TestCase {
 	protected static function getSpec() {
 		static $json;
 		if ($json == null) {
-			$version = '1.1.0';
+			$version = '1.1.1';
 			$dir = dirname(__DIR__);
 			$file = "$dir/data-v$version.json";
 			if (!is_readable($file)) {
